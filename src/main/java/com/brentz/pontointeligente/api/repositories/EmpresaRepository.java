f689cb163d591918a7b36a70729a49cc14c009ef
carlosbrentz <carlosbrentz@gmail.com>
2019-06-10 08:41:25 -0300

    incluir dependencias e classes

---
+++ src/main/java/com/brentz/pontointeligente/api/repositories/EmpresaRepository.java
@@ -0,0 +1,16 @@
+package com.brentz.pontointeligente.api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.brentz.pontointeligente.api.entities.Empresa;
+
+
+
+public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
+	
+	@Transactional(readOnly = true)
+	Empresa findByCnpj(String cnpj);
+
+}
+	
